80c5dc918c6e27b34fdc98708a9610d7f04b44fe
sdm2345 <sdm2345@163.com>
2026-07-07 12:26:32 +0800

    feat: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Practice } from './pages/Practice'
 import { Exam } from './pages/Exam'
 import { ErrorBook } from './pages/ErrorBook'
+import { Review } from '@/pages/Review'
+import Stats from '@/pages/Stats'
 import { seedIfEmpty } from './db/seed'
 
 function AppRoutes() {
@@ -20,6 +22,8 @@ function AppRoutes() {
           <Route path="/practice" element={<Practice />} />
           <Route path="/exam" element={<Exam />} />
           <Route path="/errors" element={<ErrorBook />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/stats" element={<Stats />} />
         </Routes>
       </main>
       <NavBar />
@@ -29,8 +33,8 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
